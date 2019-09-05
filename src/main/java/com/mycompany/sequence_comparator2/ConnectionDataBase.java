1ad49154203238b311401c6b3a3392054af9d27d
Gabriel Brunet <brunetgabriel@MacBook-Pro-de-Gabriel-2.local>
2019-09-05 14:38:20 +0200

    First version of data base connector class

---
+++ src/main/java/com/mycompany/sequence_comparator2/ConnectionDataBase.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sequence_comparator2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Gabriel BRUNET & Clement SAURY
+ */
+public class ConnectionDataBase {
+    private final Connection con;
+    private Statement stmt;
+    
+    public ConnectionDataBase() throws ClassNotFoundException, SQLException{
+        // Load the driver class & create the connection object then create the statement of the object
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection(
+                "jdbc:mysql://remotemysql.com:3306/jeho8etZte", "jeho8etZte", "GxTGVa1AvL");
+        stmt = con.createStatement();
+    }
+    
+    public void shutdown() throws SQLException {
+        if (con != null) {
+            con.close();
+        }
+    }
+    
+    public Connection getCon() {
+        return con;
+    }
+    
+    public Statement getStatement(){
+            return stmt;
+    }
+}
