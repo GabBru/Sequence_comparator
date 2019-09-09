8aeb457cf90954a119542ca35ed9e0f0b0a4d470
gphy <gphy@b2-info2-02>
2019-09-09 15:16:04 +0200

    clean code

---
+++ src/main/java/com/mycompany/sequence_comparator2/Generate_tree.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sequence_comparator2;
+import java.io.IOException;
+import org.forester.archaeopteryx.Archaeopteryx;
+import org.forester.phylogeny.Phylogeny;
+/**
+ *
+ * @author gphy
+ */
+public class Generate_tree {
+    public void Generate_tree(){
+    
+}
+}
