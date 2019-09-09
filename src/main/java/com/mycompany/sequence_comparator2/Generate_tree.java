53d4af0ad167079b0cdd7536d4ce4b51ef7c734b
gphy <gphy@b2-info2-02>
2019-09-09 15:51:47 +0200

    clean code

--- src/main/java/com/mycompany/sequence_comparator2/Generate_tree.java
+++ src/main/java/com/mycompany/sequence_comparator2/Generate_tree.java
@@ -4,15 +4,41 @@
  * and open the template in the editor.
  */
 package com.mycompany.sequence_comparator2;
+
+import static com.mycompany.sequence_comparator2.Clustal.LOGGER;
+import java.io.File;
 import java.io.IOException;
-import org.forester.archaeopteryx.Archaeopteryx;
-import org.forester.phylogeny.Phylogeny;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  *
  * @author gphy
  */
 public class Generate_tree {
+    String tree;
+    Clustal cl = new Clustal();
     public void Generate_tree(){
-    
+        tree = cl.getTree();
 }
+    
+    public void submit() throws IOException, InterruptedException{
+     // Set the path of the driver to driver executable. For Chrome, set the properties as following:       
+        File file = new File(System.getProperty("user.dir")+"/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        
+        // Create a Chrome Web Driver with visual
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        // Open the clustalo homepage
+        driver.get("http://phylo.io/#");
+        //add all lines in the input 
+        driver.findElement(By.id("newickInSingle")).sendKeys(tree);
+        //click on submit button 
+        driver.findElement(By.id("renderButton")).submit();
+    }
 }
